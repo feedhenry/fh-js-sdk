33d20c37092fd8947c672cf311b592eae0f1833e
david-martin <david.martin@feedhenry.com>
2012-08-29 13:51:49 +0200

    Bump version

--- feedhenry.js
+++ feedhenry.js
@@ -491,7 +491,7 @@ if (!JSON) {
   var $fh = root.$fh;
   $fh.fh_timeout = 20000;
   $fh.boxprefix = '/box/srv/1.1/';
-  $fh.sdk_version = '1.0.0';
+  $fh.sdk_version = '1.0.1';
   
   var _is_initializing = false;
   var _cloud_ready_listeners = [];
