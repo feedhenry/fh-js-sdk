4f39e2e2d38cd65dd186fd38b0469703413dc655
John Frizelle <john.frizelle@feedhenry.com>
2012-09-13 11:38:47 +0100

    Bump version number to 1.0.4

--- feedhenry.js
+++ feedhenry.js
@@ -491,7 +491,7 @@ if (!JSON) {
   var $fh = root.$fh;
   $fh.fh_timeout = 20000;
   $fh.boxprefix = '/box/srv/1.1/';
-  $fh.sdk_version = '1.0.3';
+  $fh.sdk_version = '1.0.4';
   
   var _is_initializing = false;
   var _cloud_ready_listeners = [];
