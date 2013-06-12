ea5a8e899f0da6e6017313164416bbf5d683a3de
Jason Madigan <jason.madigan@feedhenry.com>
2013-06-12 15:20:22 +0100

    Bump version

--- dist/feedhenry-latest.js
+++ dist/feedhenry-latest.js
@@ -5561,7 +5561,7 @@ Lawnchair.adapter('webkit-sqlite', (function() {
   var $fh = root.$fh;
   $fh.fh_timeout = 20000;
   $fh.boxprefix = '/box/srv/1.1/';
-  $fh.sdk_version = '1.1.0';
+  $fh.sdk_version = '1.1.1';
   
   var _is_initializing = false;
   var _init_failed = false;
