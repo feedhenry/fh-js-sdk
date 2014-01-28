8597f9a2d2d997632ada2fd00947500383877545
Niall Donnelly <niall.donnelly@feedhenry.com>
2014-01-28 09:59:45 +0000

    Fixed merge error

--- src/appforms/src/core/040-Model08Page.js
+++ src/appforms/src/core/040-Model08Page.js
@@ -28,6 +28,71 @@ appForm.models = function (module) {
       this.emit('hidden');
     }
   };
+  Page.prototype.getFieldDef=function(){
+    return this.get("fields",[]);
+  };
+  Page.prototype.getFieldModelList=function(){
+    console.log("fieldModelList", this.fieldsIds);
+    var list=[];
+    for (var i=0;i<this.fieldsIds.length;i++){
+      list.push(this.form.getFieldModelById(this.fieldsIds[i]));
+    }
+    return list;
+  };
+  Page.prototype.checkForSectionBreaks=function(){ //Checking for any sections
+    for (var i=0;i<this.fieldsIds.length;i++){
+      var fieldModel = this.form.getFieldModelById(this.fieldsIds[i]);
+      if(fieldModel.getType() == "sectionBreak"){
+        return true;
+      }
+    }
+
+    return false;
+  };
+  Page.prototype.getSections=function(){ //Checking for any sections
+    var sectionList={};
+    var currentSection = null;
+    var sectionBreaksExist = this.checkForSectionBreaks();
+    var insertSectionBreak = false;
+
+    if(sectionBreaksExist){
+      //If there are section breaks, the first field in the form must be a section break. If not, add a placeholder
+      var firstField = this.form.getFieldModelById(this.fieldsIds[0]);
+
+      if(firstField.getType() != "sectionBreak"){
+        insertSectionBreak = true;
+      }
+    } else {
+      return null;
+    }
+
+    for (var i=0;i<this.fieldsIds.length;i++){
+      var fieldModel = this.form.getFieldModelById(this.fieldsIds[i]);
+
+      if(insertSectionBreak && i == 0){ //Adding a first section.
+        currentSection = "sectionBreak" + i;
+        sectionList[currentSection] = sectionList[currentSection] ? sectionList[currentSection] : [];
+      }
+
+      if(currentSection !== null && fieldModel.getType() != "sectionBreak"){
+        sectionList[currentSection].push(fieldModel);
+      }
+
+      if(fieldModel.getType() == "sectionBreak"){
+        currentSection = "sectionBreak" + i;
+        sectionList[currentSection] = sectionList[currentSection] ? sectionList[currentSection] : [];
+        sectionList[currentSection].push(fieldModel);
+      }
+    }
+
+    return sectionList;
+  };
+  Page.prototype.getFieldModelById=function(fieldId){
+    return this.form.getFieldModelById(fieldId);
+  };
+  Page.prototype.getPageId=function(){
+    return this.get("_id","");
+  };
   Page.prototype.getName = function () {
     return this.get('name', '');
   };
@@ -43,82 +108,11 @@ appForm.models = function (module) {
     for (var i = 0; i < this.fieldsIds.length; i++) {
       list.push(this.form.getFieldModelById(this.fieldsIds[i]));
     }
-    Page.prototype.getFieldDef=function(){
-        return this.get("fields",[]);
-    }
-    Page.prototype.getFieldModelList=function(){
-        console.log("fieldModelList", this.fieldsIds);
-        var list=[];
-        for (var i=0;i<this.fieldsIds.length;i++){
-            list.push(this.form.getFieldModelById(this.fieldsIds[i]));
-        }
-        return list;
-    }
-    Page.prototype.checkForSectionBreaks=function(){ //Checking for any sections
-      for (var i=0;i<this.fieldsIds.length;i++){
-        var fieldModel = this.form.getFieldModelById(this.fieldsIds[i]);
-        if(fieldModel.getType() == "sectionBreak"){
-          return true;
-        }
-      }
 
-      return false;
-    }
-    Page.prototype.getSections=function(){ //Checking for any sections
-      var sectionList={};
-      var currentSection = null;
-      var sectionBreaksExist = this.checkForSectionBreaks();
-      var insertSectionBreak = false;
+    return list;
+  };
 
-      if(sectionBreaksExist){
-        //If there are section breaks, the first field in the form must be a section break. If not, add a placeholder
-        var firstField = this.form.getFieldModelById(this.fieldsIds[0]);
-
-        if(firstField.getType() != "sectionBreak"){
-          insertSectionBreak = true;
-        }
-      } else {
-        return null;
-      }
-
-      for (var i=0;i<this.fieldsIds.length;i++){
-        var fieldModel = this.form.getFieldModelById(this.fieldsIds[i]);
-
-        if(insertSectionBreak && i == 0){ //Adding a first section.
-          currentSection = "sectionBreak" + i;
-          sectionList[currentSection] = sectionList[currentSection] ? sectionList[currentSection] : [];
-        }
-
-        if(currentSection !== null && fieldModel.getType() != "sectionBreak"){
-          sectionList[currentSection].push(fieldModel);
-        }
-
-        if(fieldModel.getType() == "sectionBreak"){
-          currentSection = "sectionBreak" + i;
-          sectionList[currentSection] = sectionList[currentSection] ? sectionList[currentSection] : [];
-          sectionList[currentSection].push(fieldModel);
-        }
-      }
-
-      return sectionList;
-    }
-    Page.prototype.getFieldModelById=function(fieldId){
-        return this.form.getFieldModelById(fieldId);
-    }
-    Page.prototype.getPageId=function(){
-        return this.get("_id","");
-    }
     module.Page=Page;
 
     return module;
-})(appForm.models || {});    return list;
-  };
-  Page.prototype.getFieldModelById = function (fieldId) {
-    return this.form.getFieldModelById(fieldId);
-  };
-  Page.prototype.getPageId = function () {
-    return this.get('_id', '');
-  };
-  module.Page = Page;
-  return module;
 }(appForm.models || {});
