1debdb380714fee24f9a0f854a5efb039917b437
Niall Donnelly <niall.donnelly@feedhenry.com>
2014-04-10 15:59:57 +0100

    Latest js-sdk generation

--- libs/generated/appForms/appForms-core.js
+++ libs/generated/appForms/appForms-core.js
@@ -5643,9 +5643,9 @@ if (typeof $fh == 'undefined') {
 if ($fh.forms === undefined) {
   $fh.forms = appForm.api;
 }
-/*! fh-forms - v0.3.01 -  */
+/*! fh-forms - v0.5.3 -  */
 /*! async - v0.2.9 -  */
-/*! 2014-04-02 */
+/*! 2014-04-10 */
 /* This is the prefix file */
 if(appForm){
   appForm.RulesEngine=rulesEngine;
