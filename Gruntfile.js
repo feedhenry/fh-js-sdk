8a1c8be9db1d44238b57cd44e4569fce264e4946
Wei Li <wei.li@feedhenry.com>
2014-06-17 21:50:50 +0100

    update grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -132,9 +132,12 @@ module.exports = function(grunt) {
               var t = data;
               if(file.indexOf("constants.js") >= 0){
                 var version = pkg.version;
+                console.log("found current version = " + version);
                 if(process.env.BUILD_NUMBER){
+                  console.log("found BUILD_NUMBER in process.env " + process.env.BUILD_NUMBER);
                   version = version.replace(/BUILD\-NUMBER/g, process.env.BUILD_NUMBER);
                 }
+                console.log("Version to inject is " + version);
                 t = data.replace("BUILD_VERSION", version);
               }
               this.queue(t);
