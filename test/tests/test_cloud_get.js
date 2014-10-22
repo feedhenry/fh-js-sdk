74f1c2d3d243136fdb80d6260ce4ddc02b03c769
Martin Murphy <martin.murphy@feedhenry.com>
2014-10-22 13:56:04 +0100

    Add test for fh.cloud GET api - 8186

---
+++ test/tests/test_cloud_get.js
@@ -0,0 +1,94 @@
+var chai = require('chai');
+var expect = chai.expect;
+var sinonChai = require('sinon-chai');
+var url = require('url');
+
+var process = require("process");
+if(document && document.location){
+  if(document.location.href.indexOf("coverage=1") > -1){
+    process.env.LIB_COV = 1;
+  }
+}
+
+var ajax = process.env.LIB_COV? require("../../src-cov/modules/ajax") : require("../../src/modules/ajax");
+var qs = process.env.LIB_COV? require("../../src-cov/modules/queryMap"): require("../../src/modules/queryMap");
+
+chai.use(sinonChai);
+
+var fhconfig = {
+  "host": "http://localhost:8100",
+  "appid" : "testappid",
+  "appkey" : "testappkey",
+  "projectid" : "testprojectid",
+  "connectiontag" : "testconnectiontag"
+};
+
+var apphost = {
+  domain: "testing",
+  firstTime: false,
+  hosts: {
+    "url": "http://localhost:8101"
+  },
+  init: {
+    "trackId": "testtrackid"
+  }
+};
+
+var expectedUrl = "http://localhost:8101";
+
+if(document && document.location){
+  var doc_url = document.location.href;
+  var url_params = qs(doc_url);
+  var local = (typeof url_params.url !== 'undefined');
+  if(local){
+    expectedUrl = url_params.url;
+  }
+}
+
+describe("test all cloud related GETs", function(){
+
+  var server;
+  var requests;
+
+  beforeEach(function () {
+    server = sinon.useFakeXMLHttpRequest(); 
+    requests = [];
+    server.onCreate = function (xhr) {
+      requests.push(xhr);
+    };
+  });
+
+  afterEach(function () { server.restore(); });
+
+  it("should work with cloud GET call", function(){
+    var success = sinon.spy();
+    var fail = sinon.spy();
+
+    var $fh = process.env.LIB_COV? require("../../src-cov/feedhenry") : require("../../src/feedhenry");
+
+    $fh.reset();
+    $fh.cloud({
+      path: 'test/echo',
+      method: 'GET',
+      data: {var1: 'hello'}
+    }, success, fail);
+
+    expect(requests).to.have.length.of.at.least(1);
+    var request = requests[0];
+    expect(request.method).to.equal('GET');
+
+    expect(request).to.have.property('url');
+
+    var requestedUrl = url.parse(request.url, true);
+    expect(requestedUrl.pathname).to.equal('/test/echo');
+    expect(requestedUrl.query).to.have.property('var1', 'hello');
+
+    request.respond(200, { "Content-Type": "application/json" }, '{ "status": "it worked!"}');
+
+    expect(success).to.have.been.calledOnce;
+    expect(success).to.have.been.calledWith({ "status": "it worked!"});
+    expect(fail).to.have.not.been.called;
+
+  });
+});
+
