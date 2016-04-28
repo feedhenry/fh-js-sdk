4cb1a56b25333a4922d204510a46324fece17961
Erik Jan de Wit <erikjan.dewit@gmail.com>
2016-04-28 17:16:16 +0200

    changes in file api

--- src/appforms/src/core/000-api-v2.js
+++ src/appforms/src/core/000-api-v2.js
@@ -2007,17 +2007,15 @@
   };
 
   if (typeof(window.PhoneGap) !== 'undefined' || typeof(window.cordova) !== 'undefined') {
-    $fh._readyCallbacks = [];
-    $fh._readyState = false;
     $fh.__dest__.ready = function(p, s, f) {
-      if ($fh._readyState) {
+      if (__is_ready) {
         try {
           s();
         } catch (e) {
           console.log("Error during $fh.ready. Skip. Error = " + e.message);
         }
       } else {
-        $fh._readyCallbacks.push(s);
+        __ready_list.push(s);
       }
     };
   }
