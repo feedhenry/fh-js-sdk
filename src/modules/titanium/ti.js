72b5d98bafb68ddd757829e714b30232cccb0dbd
Erik Jan de Wit <erikjan.dewit@gmail.com>
2016-05-19 11:19:06 +0200

    added wrapper around AeroGear-Push-Titanium module

--- src/modules/titanium/ti.js
+++ src/modules/titanium/ti.js
@@ -8,4 +8,22 @@ if (typeof Titanium !== 'undefined'){
   if (typeof navigator === 'undefined'){
     navigator = { userAgent : 'Titanium' };
   }
+  
+  window.push = {
+    register: function (onNotification, success, fail, config) {
+      var push = require('AeroGearPush').init(config);
+      push.registerDevice({
+        extraOptions: {
+          categories: config.categories,
+          alias: config.alias
+        },
+        onReceive: function (event) {
+		  onNotification(event);
+        },
+        onTokenSuccess: function () {
+        	success();
+        }
+      });
+    }
+  };
 }
